2d5e4821d67638cecdb5c80d7743677d7a3d30b5
Minsub Won <jbk4860@skku.edu>
2022-05-11 00:07:39 +0900

    Update infer_with_docker.sh

--- scripts/infer_with_docker.sh
+++ scripts/infer_with_docker.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-#####################
-# Parameter Setting #
-#####################
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
-
-MAIN_PATH="/home1/irteam/users/minsub/for_dev/minsub_git/torch_transformer"
+MAIN_PATH="${YOUR_GIT_REPO_PATH}/torch_transformer"
 PACKAGE_PATH="${MAIN_PATH}/koen.2022.0505"
 SRC_LANG="ko"
 TGT_LANG="en"
 BATCH_SIZE="4"
 CORPUS_PATH="${MAIN_PATH}/test_data/test.ko"
 DEVICE="cpu"
-##############
-# Run Script #
-##############
 
 docker run -it \
     -v ${MAIN_PATH}:${MAIN_PATH} \
